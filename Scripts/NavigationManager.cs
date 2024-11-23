53135b1f925a77d65365c8b7ccd9221ba2010724
manishchilwal <160496665+manishchilwal@users.noreply.github.com>
2024-11-23 06:46:15 +0530

    Initial commit

--- Scripts/NavigationManager.cs
+++ Scripts/NavigationManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0886218a76fc0656aa10bc5ca6d34ab9f58e4d2d61adabc76306736ac9ba29e6
-size 2568
+oid sha256:5883e2a2fb48b372a7c38493bb00dc1593920cdb547a8708e5949999d7a5389c
+size 3427
